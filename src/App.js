6a4ac30cd30d4d5c2cf7cc29ccbcc134d29f38b2
rlaplaud <ronan.laplaud@gfi.world>
2020-02-04 11:33:39 +0100

    init repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import {PostgrestClient} from '@supabase/postgrest-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    useEffect(() => {
+        console.log('app did mount');
+        const client = new PostgrestClient('http://localhost:4000');
+        client
+            .from('omega')
+            .select('*')
+            .then(res => {
+               const {body: users} = res;
+               console.log(users);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+        client
+            .from('omega')
+            .match({
+                id: '15f65a8a-3d84-4a64-add6-d00bb1b698c4'
+            })
+            .update({
+                username: 'applast modified'
+            }, {
+                bulk: false
+            }).then(res => {
+                console.log(res);
+            }).catch(e => {
+                console.log(e);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+        </div>
+    );
+
+};
 
 export default App;
