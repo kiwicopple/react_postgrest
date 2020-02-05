6347d3ec74ccb07b983218beefa62862e151f72e
Paul Copplestone <pcopplestone@gmail.com>
2020-02-05 15:52:43 +0800

    Testing with other libs

--- src/App.js
+++ src/App.js
@@ -1,44 +1,68 @@
-import React, {useEffect} from 'react';
-import './App.css';
-import {PostgrestClient} from '@supabase/postgrest-js';
+import React, { useEffect } from "react";
+import "./App.css";
+import { PostgrestClient } from "@supabase/postgrest-js";
+// import axios from "axios";
+import superagent from "superagent";
 
 const App = () => {
+  useEffect(() => {
+    console.log("app did mount");
+    const client = new PostgrestClient("http://localhost:4000");
+    client
+      .from("omega")
+      .select("*")
+      .then(res => {
+        const { body: users } = res;
+        console.log(users);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+      
+    client
+      .from("omega")
+      .match({
+        id: "kiwicopple"
+      })
+      .update(
+        {
+          username: "supabase"
+        },
+        {
+          "Content-Type": "application/json",
+          'accept': 'json'
+        }
+      )
+      .then(res => {
+        console.log(res);
+      })
+      .catch(e => {
+        console.log(e);
+      });
 
-    useEffect(() => {
-        console.log('app did mount');
-        const client = new PostgrestClient('http://localhost:4000');
-        client
-            .from('omega')
-            .select('*')
-            .then(res => {
-               const {body: users} = res;
-               console.log(users);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-        client
-            .from('omega')
-            // .match({
-            //     id: 'ki'
-            // })
-            .update({
-                username: 'applast modified'
-            }, {
-                bulk: false
-            })
-            .then(res => {
-                console.log(res);
-            }).catch(e => {
-                console.log(e);
-            });
-    }, []);
+    // axios
+    //   .patch("http://localhost:4000/omega?id=eq.kiwicopple", {
+    //     username: "axios"
+    //   })
+    //   .then(res => {
+    //     console.log(res);
+    //   })
+    //   .catch(e => {
+    //     console.log(e);
+    //   });
 
-    return (
-        <div className="App">
-        </div>
-    );
+    // superagent
+    //   .patch("http://localhost:4000/omega?id=eq.kiwicopple")
+    //   .send({
+    //     username: "superagent"
+    //   })
+    //   .end((err, res) => {
+    //     console.log("err", err);
+    //     console.log("res", res);
+    //   });
+  }, []);
 
+  return <div className="App"></div>;
 };
 
 export default App;
